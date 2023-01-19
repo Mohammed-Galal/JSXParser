7311c7fa2f1c2f2b19e5130d772f672e9b817065
Mohammed Galal <55014084+Mohammed-Galal@users.noreply.github.com>
2023-01-19 23:47:46 +0200

    .

--- scriptsCatcher.js
+++ scriptsCatcher.js
@@ -1,62 +1,54 @@
-import { cut, emptyStr } from "./commonAssets.js";
+import { emptyStr } from "./commonAssets.js";
 
 export default (function () {
-  const openScriptTag = "{",
-    closingScriptTag = "}",
-    openScriptTagExp = /\{/;
+  const splitterExp = /(?<=\{)|(?=\})/g,
+    openScriptTagExp = /\{$/,
+    closingScriptTag = /^\}/,
+    hasScripts = /\{/;
 
   let scripts = [],
     scriptHolder = [],
     pureContent = [];
 
-  let str = null,
+  let domArr = null,
     endPos = null;
 
   let currentPos = 0,
     openScripts = 0;
 
-  return function ($str) {
-    if (openScriptTagExp.test($str)) {
-      str = $str;
-      endPos = $str.length;
+  return function (str) {
+    if (hasScripts.test(str)) {
+      domArr = str.split(splitterExp);
+      endPos = str.length;
       catchScript();
       const _Scripts = scripts,
         rawContent = pureContent;
       reset();
-
       return {
         scripts: _Scripts,
         input: rawContent.join(emptyStr),
       };
-    } else return { scripts, input: $str };
+    } else return { scripts, input: str };
   };
 
   function catchScript() {
-    if (openScripts === 0) {
-      const startPos = currentPos;
-      currentPos = str.indexOf(openScriptTag, startPos);
-      const isExisted = currentPos > -1;
-      pureContent.push(str[cut](startPos, isExisted ? ++currentPos : endPos));
-      if (isExisted === false) return;
-      openScripts = 1;
+    const item = domArr[currentPos++];
+
+    if (openScripts === 0) pureContent.push(item);
+    else {
+      if (closingScriptTag.test(item) && --openScripts === 0) {
+        const result = scriptHolder.join(emptyStr);
+        const resIndex = scripts.indexOf(result);
+        scriptHolder.length = 0;
+        pureContent.push(
+          resIndex > -1 ? resIndex : scripts.push(result) - 1,
+          item
+        );
+      } else scriptHolder.push(item);
     }
 
-    const item = str[currentPos++];
-
-    if (item === closingScriptTag && --openScripts === 0) {
-      const result = scriptHolder.join(emptyStr);
-      const resIndex = scripts.indexOf(result);
-      scriptHolder.length = 0;
-      pureContent.push(
-        resIndex > -1 ? resIndex : scripts.push(result) - 1,
-        item
-      );
-      catchScript();
-    } else {
-      if (item === openScriptTag) openScripts++;
-      scriptHolder.push(item);
-      catchScript();
-    }
+    if (openScriptTagExp.test(item)) openScripts++;
+    if (endPos > currentPos) catchScript();
   }
 
   function reset() {
@@ -65,6 +57,6 @@ export default (function () {
     scripts = [];
     scriptHolder = [];
     pureContent = [];
-    str = endPos = null;
+    domArr = endPos = null;
   }
 })();
