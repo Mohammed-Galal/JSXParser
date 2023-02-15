facf992de0ba84e2e6ed83b542d4db4d82d517bc
Mohammed-Galal <mohammedgalal682@gmail.com>
2023-02-15 20:25:35 +0200

    remade

--- index.mjs
+++ index.mjs
@@ -2,61 +2,50 @@ import { openingTagExp, emptyStr } from "./commonAssets.mjs";
 import parse from "./xmlParser.mjs";
 
 const cut = String.prototype.slice === undefined ? "substring" : "slice",
-  isNum = Number.isInteger,
   closingTagExp = /^<\/\w|\/>$/,
   fragExp = /\<\/?\>/g,
-  rootCheckExp = /\<\w/,
   fileSplitter = /(?=\<\/?\w)|(?<=\>)/g;
+
+export default start;
+function start(content) {
+  const fragFilled = content.replace(fragExp, replacer).split(fileSplitter);
+  return fragFilled.length > 1 ? parseContent(fragFilled) : content;
+}
 
 let rootHolder = [],
   openRoots = 0;
 
-export default start;
-function start(content) {
-  const fragFilled = content.replace(fragExp, replacer),
-    balanced = rootCheckExp.test(fragFilled);
-  return balanced ? parseContent(content.split(fileSplitter)) : content;
-}
-
 function parseContent(contentArr) {
-  const roots = [],
-    raw = [];
+  const raw = [];
+  let key = 0;
+  contentArr.forEach(handleItem);
+  return raw.join(emptyStr);
 
-  contentArr.forEach(collectRoots);
-  return raw.map(filter).join(emptyStr);
-
-  function collectRoots(item) {
+  function handleItem(item) {
     if (openingTagExp.test(item)) openRoots++;
     if (openRoots > 0) {
       rootHolder.push(item);
       if (closingTagExp.test(item) && --openRoots === 0) {
-        const result = rootHolder.join(emptyStr);
-        raw.push(roots.push(result) - 1);
+        const component = parse(rootHolder.join(emptyStr)),
+          result =
+            "{\n\t" +
+            "key:" +
+            key++ +
+            ",\n\t" +
+            "scripts:[" +
+            component.scripts.map(start) +
+            "],\n\t" +
+            "components:[" +
+            component.components +
+            "],\n\t" +
+            "dom:" +
+            JSON.stringify(component.dom) +
+            "\n}";
+
         rootHolder.length = 0;
+        raw.push(result);
       }
     } else raw.push(item);
-  }
-
-  function filter(c) {
-    if (isNum(c)) {
-      const component = parse(roots[c]);
-      return (
-        "{\n\t" +
-        "key:" +
-        c +
-        ",\n\t" +
-        "scripts:[" +
-        component.scripts.map(start) +
-        "],\n\t" +
-        "components:[" +
-        component.components +
-        "],\n\t" +
-        "dom:" +
-        JSON.stringify(component.dom) +
-        "\n}"
-      );
-    }
-    return c;
   }
 }
 
