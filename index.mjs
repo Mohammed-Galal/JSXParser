13e0ebf986346690df0928fb3499a3fd63ba7e23
Mohammed-Galal <mohammedgalal682@gmail.com>
2023-02-23 15:33:40 +0200

    exps gathered

--- index.mjs
+++ index.mjs
@@ -2,24 +2,28 @@ import { openingTagExp, emptyStr } from "./commonAssets.mjs";
 import parse from "./xmlParser.mjs";
 
 const cut = String.prototype.slice === undefined ? "substring" : "slice",
-  commentsExp = /\<\!--[^]+--\>/g,
-  fragExp = /\<\/?\>/g,
-  closingTagExp = /^\<\/\w|\/\>$/,
-  rootsExp = /\<\w[\w-.:]*(?:\s*\/?\>|\s*\w\S+\=)/,
-  fileSplitter = /(?=\<\/\w)|(?=\<\w[\w-.:]*(?:\s*\/?\>|\s+\w\S*\=))|(?<=\>)/g;
+  EXP = {
+    comments: /\<\!--[^]+--\>/g,
+    fragmentTag: /\<\/?\>/g,
+    closingTag: /^\<\/\w|\/\>$/,
+    rootCheck: /\<\w[\w-.:]*(?:\s*\/?\>|\s*\w\S+\=)/,
+    fileSplit: /(?=\}|\<\/\w|\<\w[\w-.:]*(?:\s*\/?\>|\s+\w\S*\=))|(?<=\{|\>)/g,
+  };
 
 export default start;
 function start(content) {
-  const fragFilled = content.replace(fragExp, replacer),
-    hasRoots = rootsExp.test(fragFilled);
-  return hasRoots ? parseContent(fragFilled.split(fileSplitter)) : content;
+  const fragFilled = content.replace(EXP.fragmentTag, replacer),
+    hasRoots = EXP.rootCheck.test(fragFilled);
+  return hasRoots ? parseContent(fragFilled.split(EXP.fileSplit)) : content;
 }
 
 function parseContent(contentArr) {
-  const raw = [];
-  let rootHolder = [],
-    openRoots = 0,
+  const raw = [],
+    rootHolder = [];
+
+  let openRoots = 0,
     key = 0;
+
   contentArr.forEach(handleItem);
   return raw.join(emptyStr);
 
@@ -27,10 +31,10 @@ function parseContent(contentArr) {
     if (openingTagExp.test(item)) openRoots++;
     else if (openRoots === 0) return raw.push(item);
     rootHolder.push(item);
-    if (closingTagExp.test(item) && --openRoots === 0) {
+    if (EXP.closingTag.test(item) && --openRoots === 0) {
       const rootStr = rootHolder.join(emptyStr);
       if (/^\<str:/.test(rootHolder[0])) return raw.push(rootStr);
-      const component = parse(rootStr.replace(commentsExp, emptyStr)),
+      const component = parse(rootStr.replace(EXP.comments, emptyStr)),
         result =
           "({\n\t" +
           "key:" +
