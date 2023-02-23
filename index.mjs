713b418a92d015e4e8914e0a371fcb888db1e14e
Mohammed-Galal <mohammedgalal682@gmail.com>
2023-02-23 14:39:30 +0200

    _

--- index.mjs
+++ index.mjs
@@ -2,7 +2,7 @@ import { openingTagExp, emptyStr } from "./commonAssets.mjs";
 import parse from "./xmlParser.mjs";
 
 const cut = String.prototype.slice === undefined ? "substring" : "slice",
-  commentsExp = /\<\!--|--\>/g,
+  commentsExp = /\<\!--[^]+--\>/g,
   fragExp = /\<\/?\>/g,
   closingTagExp = /^\<\/\w|\/\>$/,
   rootsExp = /\<\w[\w-.:]*(?:\s*\/?\>|\s*\w\S+\=)/,
@@ -10,9 +10,7 @@ const cut = String.prototype.slice === undefined ? "substring" : "slice",
 
 export default start;
 function start(content) {
-  const fragFilled = content
-      .replace(commentsExp, emptyStr)
-      .replace(fragExp, replacer),
+  const fragFilled = content.replace(fragExp, replacer),
     hasRoots = rootsExp.test(fragFilled);
   return hasRoots ? parseContent(fragFilled.split(fileSplitter)) : content;
 }
@@ -30,7 +28,9 @@ function parseContent(contentArr) {
     else if (openRoots === 0) return raw.push(item);
     rootHolder.push(item);
     if (closingTagExp.test(item) && --openRoots === 0) {
-      const component = parse(rootHolder.join(emptyStr)),
+      const rootStr = rootHolder.join(emptyStr);
+      if (/^\<str:/.test(rootHolder[0])) return raw.push(rootStr);
+      const component = parse(rootStr.replace(commentsExp, emptyStr)),
         result =
           "({\n\t" +
           "key:" +
