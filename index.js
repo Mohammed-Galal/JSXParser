eeba3935bbad543169fec58065cb3860dce4cf84
Mohammed Galal <55014084+Mohammed-Galal@users.noreply.github.com>
2023-02-26 14:15:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-const { openingTagExp, emptyStr } = require("./commonAssets.js");
-const parse = require("./xmlParser.js");
+const { openingTagExp, emptyStr } = require("./commonAssets.js"),
+  parse = require("./xmlParser.js");
 
-const cut = String.prototype.slice === undefined ? "substring" : "slice",
+const underScore = "_",
+  repeat = (m) => underScore.repeat(m.length),
+  cut = String.prototype.slice === undefined ? "substring" : "slice",
   EXP = {
+    strings: /(?<tag>["'`])[^]*?(?<!\\)\k<tag>/g,
     comments: /\<\!--[^]+?--\>/g,
     fragmentTag: /\<\/?\>/g,
     closingTag: /^\<\/\w|\/\>$/,
@@ -13,43 +16,53 @@ const cut = String.prototype.slice === undefined ? "substring" : "slice",
 module.exports = start;
 function start(content) {
   const fragFilled = content.replace(EXP.fragmentTag, replacer),
-    hasRoots = EXP.rootCheck.test(fragFilled);
-  return hasRoots ? parseContent(fragFilled.split(EXP.fileSplit)) : content;
+    stringsRemoved = fragFilled.replace(EXP.strings, repeat),
+    roots = new RegExp(gatherRoots(fragFilled), "g");
+
+  return fragFilled.replace(roots, rootHandler);
+
+  function rootHandler(rootStr, index) {
+    if (stringsRemoved.indexOf(rootStr.replace(EXP.strings, repeat)) !== index)
+      return rootStr;
+
+    const component = parse(rootStr.replace(EXP.comments, emptyStr));
+    return (
+      "({\n\t" +
+      "key:" +
+      key++ +
+      ",\n\t" +
+      "scripts:[" +
+      component.scripts.map(start) +
+      "],\n\t" +
+      "components:[" +
+      component.components +
+      "],\n\t" +
+      "dom:" +
+      JSON.stringify(component.dom) +
+      "\n})"
+    );
+  }
 }
 
-function parseContent(contentArr) {
-  const raw = [],
+let key = 0;
+function gatherRoots(str) {
+  const contentArr = str.split(EXP.fileSplit),
+    roots = [],
     rootHolder = [];
 
-  let openRoots = 0,
-    key = 0;
+  let openRoots = 0;
 
   contentArr.forEach(handleItem);
-  return raw.join(emptyStr);
+  return roots.join("|");
 
   function handleItem(item) {
     if (openingTagExp.test(item)) openRoots++;
-    else if (openRoots === 0) return raw.push(item);
+    else if (openRoots === 0) return;
     rootHolder.push(item);
     if (EXP.closingTag.test(item) && --openRoots === 0) {
-      const rootStr = rootHolder.join(emptyStr),
-        component = parse(rootStr.replace(EXP.comments, emptyStr)),
-        result =
-          "({\n\t" +
-          "key:" +
-          key++ +
-          ",\n\t" +
-          "scripts:[" +
-          component.scripts.map(start) +
-          "],\n\t" +
-          "components:[" +
-          component.components +
-          "],\n\t" +
-          "dom:" +
-          JSON.stringify(component.dom) +
-          "\n})";
+      const rootStr = rootHolder.join(emptyStr);
       rootHolder.length = 0;
-      raw.push(result);
+      roots.push(rootStr);
     }
   }
 }
