bab57b80069cea30217b73bdf96a065e70df6ab5
Mohammed Galal <55014084+Mohammed-Galal@users.noreply.github.com>
2023-02-27 22:13:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -16,56 +16,69 @@ const underScore = "_",
 module.exports = start;
 function start(content) {
   const fragFilled = content.replace(EXP.fragmentTag, replacer),
-    stringsRemoved = fragFilled.replace(EXP.strings, repeat),
-    roots = new RegExp(gatherRoots(fragFilled), "g");
-
-  return fragFilled.replace(roots, rootHandler);
-
-  function rootHandler(rootStr, index) {
-    if (stringsRemoved.indexOf(rootStr.replace(EXP.strings, repeat)) !== index)
-      return rootStr;
-
-    const component = parse(rootStr.replace(EXP.comments, emptyStr));
-    return (
-      "({\n\t" +
-      "key:" +
-      index +
-      ",\n\t" +
-      "scripts:[" +
-      component.scripts.map(start) +
-      "],\n\t" +
-      "components:[" +
-      component.components +
-      "],\n\t" +
-      "dom:" +
-      JSON.stringify(component.dom) +
-      "\n})"
-    );
-  }
+    stringsRemoved = fragFilled.replace(EXP.strings, repeat);
+  return gatherRoots(fragFilled, stringsRemoved);
 }
 
-function gatherRoots(str) {
+function gatherRoots(str, stringsRemoved) {
   const contentArr = str.split(EXP.fileSplit),
-    roots = [],
+    CONTENT = [],
     rootHolder = [];
 
-  let openRoots = 0;
+  let openRoots = 0,
+    currentPos = 0;
 
   contentArr.forEach(handleItem);
-  return roots.join("|");
+  return CONTENT.concat(str[cut](currentPos)).join(emptyStr);
 
   function handleItem(item) {
     if (openingTagExp.test(item)) openRoots++;
     else if (openRoots === 0) return;
     rootHolder.push(item);
     if (EXP.closingTag.test(item) && --openRoots === 0) {
-      const rootStr = rootHolder.join(emptyStr);
+      const rootStr = rootHolder.join(emptyStr),
+        rootIndex = stringsRemoved.indexOf(
+          rootStr.replace(EXP.strings, repeat),
+          currentPos
+        );
+
       rootHolder.length = 0;
-      roots.push(rootStr);
+      if (rootIndex === -1) return;
+
+      const realIndex = str.indexOf(rootStr, currentPos);
+
+      if (rootIndex === realIndex) {
+        CONTENT.push(str[cut](currentPos, rootIndex));
+        currentPos = rootIndex + rootStr.length;
+        CONTENT.push(handleRoot(rootStr, rootIndex));
+      } else {
+        const lastpoint = realIndex + rootStr.length;
+        CONTENT.push(str[cut](currentPos, lastpoint));
+        currentPos = lastpoint;
+      }
     }
   }
 }
 
 function replacer(m) {
   return m[cut](0, -1) + "fragment>";
+}
+
+function handleRoot(R, index) {
+  const component = parse(R.replace(EXP.comments, emptyStr));
+  return (
+    "({\n\t" +
+    "key:" +
+    index +
+    ",\n\t" +
+    "scripts:[" +
+    component.scripts.map(start) +
+    "],\n\t" +
+    "components:[" +
+    component.components +
+    "],\n\t" +
+    "dom:" +
+    JSON.stringify(component.dom) +
+    "\n})"
+  );
 }
